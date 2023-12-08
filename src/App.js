ae6b9ce20c8d4833eb826f79f4bdf95c4eeb6501
Tibira Botto <tibira.botto@gmail.com>
2023-12-08 01:36:37 -0500

    Feature: Login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
-import './App.css';
-import Home from './components/Home';
-import Workouts from './components/Workouts';
-import Blog from './components/Blog';
-import Nutrition from './components/Nutrition';
-import Products from './components/Products';
-import Login from './components/Login';
-import MealPlan from './components/MealPlan';
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import "./App.css";
+import Home from "./components/Home";
+import Workouts from "./components/Workouts";
+import Blog from "./components/Blog";
+import Nutrition from "./components/Nutrition";
+import Products from "./components/Products";
+import Login from "./components/Login";
+import MealPlan from "./components/MealPlan";
 
 function App() {
+  let navigate = useNavigate();
+  function handleLogin(user) {
+    navigate("/home");
+  }
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login onLogin={handleLogin} />} />
+        <Route path="/home" element={<Home />} />
+
         <Route path="/workouts" element={<Workouts />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/nutrition" element={<Nutrition />} />
         <Route path="/mealplan" element={<MealPlan />} />
         <Route path="/products" element={<Products />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
 export default App;
-
-
-
---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onLogin({ username, password });
+  };
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label for="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+
+        <label for="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" class="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
