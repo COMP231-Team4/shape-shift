3544253254b347dbc69943936aeca915434936a6
Tibira Botto <tibira.botto@gmail.com>
2023-12-08 23:53:33 -0500

    bug: nutrition

--- src/components/Nutrition.js
+++ src/components/Nutrition.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
+import { useNavigate } from "react-router-dom";
 
 function Nutrition() {
-    const handleMealPlanClick = () => {
-        window.open('/mealplan');
-    };
+  let navigate = useNavigate();
+  const handleMealPlanClick = () => {
+    navigate("/mealplan");
+  };
 
-    return (
-        <div className="hero">
-            <div className="hero-content">
-                <div className="text-content">
-                    <h1>Let's get your nutrition in check!</h1>
-                    <button onClick={handleMealPlanClick}>Weekly Meal Plan</button>
-                    <table></table>
-                </div>
-                <div className="feature-image">
-                    <img src="woman-choosing-food.jpg" alt="Exercise Illustration" />
-                </div>
-            </div>
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="text-content">
+          <h1>Let's get your nutrition in check!</h1>
+          <button onClick={handleMealPlanClick}>Weekly Meal Plan</button>
+          <table></table>
         </div>
-    );
+        <div className="feature-image">
+          <img src="woman-choosing-food.jpg" alt="Exercise Illustration" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Nutrition;
